929e73933d21a0d612351faaf23581a33ee1453e
stanzapo <stan.zapo@icloud.com>
2017-10-07 09:51:48 -0700

    Update helpers.js

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-var APIKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+var APIKey = "b8f91c369ff59547cd47b931d8cbd45c:0:74653931";
 
 var helpers = {
 	runQuery: function(term, start, end){
